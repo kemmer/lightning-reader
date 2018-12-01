d1aa20fdc0a7505bbb107d20ea9680a1e03acc6a
Cristiano Kemmer <criskemmer@gmail.com>
2018-12-01 16:10:39 -0200

    Providing some examples for basic testing

---
+++ packages/unity-test/src/UnityTest/Example/RunAndWatch.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace UnityTest\Example;
+
+require __DIR__."/../../../vendor/autoload.php";
+use UnityTest\TestCase;
+use UnityTest\Assert\AssertException;
+use UnityTest\Assert\AssertCode;
+
+class TestingExample extends TestCase
+{
+    protected function configure()
+    {
+    }
+
+    protected function test_Example()
+    {
+        // Testing ErrorException
+        strpos();
+    }
+
+    protected function test_Example2()
+    {
+    }
+
+    protected function test_Example3()
+    {
+        // Testing AssertException (1)
+        throw new AssertException(AssertCode::TRUE);
+    }
+
+    protected function test_Example4()
+    {
+    }
+
+    protected function test_Example5()
+    {
+    }
+
+    protected function test_Example6()
+    {
+        // Testing AssertException (2)
+        $this->assertTrue(false);
+    }
+
+    protected function test_Example7()
+    {
+        // Testing AssertException (3)
+        $this->assertEquals(1, 2);
+    }
+
+    protected function test_Example8()
+    {
+        $this->assertTrue(true);
+    }
+
+    protected function test_Example9()
+    {
+        $this->assertEquals(6, 6);
+    }
+}
+
+$te = new TestingExample;
+$te->performTesting();
