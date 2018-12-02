f0bcbf7ff71f3bfdff35d6b564b7b1e014e3901d
Cristiano Kemmer <criskemmer@gmail.com>
2018-12-02 16:27:36 -0200

    Providing additional checking and strict typing

--- src/LightningReader/Parser/Tokenizer.php
+++ src/LightningReader/Parser/Tokenizer.php
@@ -9,8 +9,11 @@ class Tokenizer
      * unit: Smallest text unit possible (character)
      * block: One or more units arranged in a sequence (string)
      */
-    public function compare($unit, $lookFor)
+    public function compare(string $unit, $lookFor) : bool
     {
+        if(is_null($lookFor) || strlen($unit) === 0)
+            return false;
+
         if(!is_array($lookFor))
             $lookFor = [$lookFor];
 
@@ -22,12 +25,17 @@ class Tokenizer
         return false;
     }
 
-    public function compareSingle($unit, $lookFor)
+    public function compareSingle(string $unit, string $lookFor) : bool
     {
+        /**
+         * This comparison is usually faster than using strcmp()
+         * and its good as we are just interested in boolean values,
+         * not about the size of comparisons
+         */
         return ($unit === $lookFor);
     }
 
-    public function bundle($block, $lookFor = null, bool $alwaysStopEOL = true)
+    public function bundle(string $block, $lookFor = null, bool $alwaysStopEOL = true) : string
     {
         $result = "";
 
