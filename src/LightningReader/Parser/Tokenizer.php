7c09a5d84ea3167f2da2b57917d5e4e45aabecf8
Cristiano Kemmer <criskemmer@gmail.com>
2018-12-03 00:26:01 -0200

    Return types and comments for Tokenizer

--- src/LightningReader/Parser/Tokenizer.php
+++ src/LightningReader/Parser/Tokenizer.php
@@ -7,10 +7,12 @@ use LightningReader\Parser\Comparator;
 
 class Tokenizer
 {
-    public function swallow(&$stream, $stopOn = null, bool $alwaysStopOnEOL = true)
+    public function swallow(&$stream, $stopOn = null, bool $alwaysStopOnEOL = true) : string
     {
         $result = "";
 
+        // Use strict comparison for fgetc() as some input (like '0')
+        // could be implicitly casted to false
         while( ($unit = fgetc($stream)) !== false)
         {
             if(Comparator::compare($unit, $stopOn)
@@ -23,7 +25,7 @@ class Tokenizer
         return $result;
     }
 
-    public function bundle(&$stream, array $delimiters)
+    public function bundle(&$stream, array $delimiters) : string
     {
         // Advance until reaches the beginning of our
         // token to be bundled
